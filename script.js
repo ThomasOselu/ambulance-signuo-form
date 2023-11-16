90e365a025bbef312c36ce4d807b59eab69a0f1d
ThomasOselu <117803665+ThomasOselu@users.noreply.github.com>
2023-11-16 17:33:20 +0300

    login form

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+});
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  const form = document.getElementById('form');
+
+  if (form.style.visibility === 'hidden') {
+    form.style.visibility = 'visible';
+  } else {
+    form.style.visibility = 'hidden';
+  }
+});
+
